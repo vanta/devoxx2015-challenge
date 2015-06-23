91055b8cb6733c5730e83f714fa79e639392e5aa
Krzysztof Wolny <krzysztof.wolny@sabre.com>
2015-06-23 23:55:18 +0200

    Implemented OfferService class

--- src/main/java/pl/allegro/promo/devoxx2015/application/OfferService.java
+++ src/main/java/pl/allegro/promo/devoxx2015/application/OfferService.java
@@ -1,6 +1,7 @@
 package pl.allegro.promo.devoxx2015.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import pl.allegro.promo.devoxx2015.domain.Offer;
 import pl.allegro.promo.devoxx2015.domain.OfferRepository;
@@ -8,9 +9,11 @@ import pl.allegro.promo.devoxx2015.domain.PhotoScoreSource;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
 @Component
 public class OfferService {
-
     private final OfferRepository offerRepository;
     private final PhotoScoreSource photoScoreSource;
 
@@ -21,10 +24,27 @@ public class OfferService {
     }
 
     public void processOffers(List<OfferPublishedEvent> events) {
-        // TODO save offers with pretty photos
+        final List<Offer> offers = events.stream()
+                .map(this::createOffer)
+                .filter(Offer::hasPrettyPhoto)
+                .collect(toList());
+
+        offerRepository.save(offers);
+    }
+
+    private Offer createOffer(OfferPublishedEvent ope) {
+        return new Offer(ope.getId(), ope.getTitle(), ope.getPhotoUrl(), getScoreForPhoto(ope.getPhotoUrl()));
+    }
+
+    private double getScoreForPhoto(String photoUrl) {
+        try {
+            return photoScoreSource.getScore(photoUrl);
+        } catch (Exception e) {
+            return 0.7d;
+        }
     }
 
     public List<Offer> getOffers() {
-        return offerRepository.findAll(); // TODO some sorting?
+        return offerRepository.findAll(new Sort(DESC, "photoScore"));
     }
 }
